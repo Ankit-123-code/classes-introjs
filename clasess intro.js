ff595a0a30e55eb6fe5a85bc5ddaa3536f59c5a3
ANKIT <Ankitnayal07@gmail.com>
2024-10-17 20:55:38 +0530

    update

---
+++ clasess intro.js
@@ -0,0 +1,14 @@
+class Human{
+legs;
+arms;
+occupation;
+task;
+constructor(legs,arms,occupation,task){
+    this.legs=legs;
+    this.arms=arms;
+    this.occupation= occupation;
+    this.task =task;
+}
+}
+let Human1=new Human(2,2,"Manager",car)
+console.log(Human1);
